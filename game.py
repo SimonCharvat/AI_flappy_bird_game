e0ccd1f24f138216304ac707b04ee63a0534fbcc
Simon Charvat <124459114+SimonCharvat@users.noreply.github.com>
2023-03-29 12:07:54 +0200

    Frontend app code to class
    Transformed Tkinter empty app's code as a class

--- game.py
+++ game.py
@@ -1,45 +1,52 @@
 import tkinter as tk
 
 
+class App():
+    
+    def __init__(self):
 
-ROOT_WIDTH = 1000
-ROOT_HEIGHT = 600
+        self.ROOT_WIDTH = 1000
+        self.ROOT_HEIGHT = 600
 
-CANVAS_WIDTH = 800
-CANVAS_HEIGHT = 500
-
-
-COLOR_BACKGROUND = "#000000"  # Black
-COLOR_GRID = "#999999"  # Grey
-COLOR_SNAKE_BODY = "#077309"  # Dark green
-COLOR_APPLE = "#c20000"  # Dark red
-
-TIME_DRAW_INTERVAL_MS = 10
-TIME_ENGINE_INTERVAL_MS = 500
+        self.CANVAS_WIDTH = 800
+        self.CANVAS_HEIGHT = 500
 
 
+        self.COLOR_BACKGROUND = "#000000"  # Black
+        self.COLOR_GRID = "#999999"  # Grey
+        self.COLOR_SNAKE_BODY = "#077309"  # Dark green
+        self.COLOR_APPLE = "#c20000"  # Dark red
 
-root = tk.Tk()
-root.title("Snake")
-root.resizable(False, False)
-root.geometry(f"{ROOT_WIDTH}x{ROOT_HEIGHT}")
+        self.TIME_DRAW_INTERVAL_MS = 10
+        self.TIME_ENGINE_INTERVAL_MS = 500
 
 
 
-canvas = tk.Canvas(background=COLOR_BACKGROUND)
-canvas.config(width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-canvas.pack()
+        self.root = tk.Tk()
+        self.root.title("Flappy bird AI")
+        self.root.resizable(False, False)
+        self.root.geometry(f"{self.ROOT_WIDTH}x{self.ROOT_HEIGHT}")
 
-def engine_loop():
-    root.after(TIME_ENGINE_INTERVAL_MS, engine_loop)
 
-def draw_loop():
-    canvas.pack()
-    root.after(TIME_DRAW_INTERVAL_MS, draw_loop)
+
+        self.canvas = tk.Canvas(background=self.COLOR_BACKGROUND)
+        self.canvas.config(width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT)
+        self.canvas.pack()
+
+    def engine_loop(self):
+        self.root.after(self.TIME_ENGINE_INTERVAL_MS, self.engine_loop)
+
+    def draw_loop(self):
+        self.canvas.pack()
+        self.root.after(self.TIME_DRAW_INTERVAL_MS, self.draw_loop)
+
+
 
 
 if __name__ == "__main__":
-    engine_loop()
-    draw_loop()
+    app = App()
 
-    root.mainloop()
+    app.engine_loop()
+    app.draw_loop()
+
+    app.root.mainloop()
